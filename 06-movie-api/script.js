abb70935596f2bad8a40dabe4d54159f5dcb85d3
chevonnelise <elise.chevonne@gmail.com>
2022-11-21 19:44:48 +0800

    movie api

--- 06-movie-api/script.js
+++ 06-movie-api/script.js
@@ -1,5 +1,17 @@
+const BASE_API_URL="https://www.ombapi.com/";
+
 document.querySelector("#searchBtn")
-    .addEventListener("click", function() {
-        let tile = document.querySelector("#title");
-        let type = document.querySelector(".type:checked");
+    .addEventListener("click", async function() {
+        let title = document.querySelector("#title").value;
+        // select the element with class "type" and is checked
+        let type = document.querySelector(".type:checked").value;
+
+        const response = await axios.get(BASE_API_URL,{
+            params:{
+                "apikey":"1891b5a6",
+                "s": title,
+                "type":type
+            }
+        })
+        console.log(response.data);
     })
